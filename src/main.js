32cb6d2f53868ad4817dfd3ef5c0c2997d48ee98
iduk <iduk.design@gmail.com>
2021-09-04 18:54:22 +0900

    fix Vue3 => Vue2 Migration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
+import cssVars from "css-vars-ponyfill";
 import './assets/index.css'
+
+
+cssVars({
+  // DEMO: Process only demo.css file
+  include: 'style[id*="index.css"]',
+  // DEMO: Treat modern browsers as legacy
+  onlyLegacy: false,
+
+  // DEMO: Toggles to see results
+  // ----------------------------------------
+  // preserveStatic: false,
+  // preserveVars: true,
+  // updateURLs: false,
+  // variables: { '--color': 'purple' },
+  // ----------------------------------------
+});
+
 
 Vue.config.productionTip = false
 
