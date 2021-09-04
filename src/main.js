2efcd511d8aa1f01ee81b284e7f8f6c43bb4f24a
iduk <iduk.design@gmail.com>
2021-09-04 17:50:27 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/index.css'
-import 'babel-polyfill'
+import '@babel/polyfill'
 
 createApp(App).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+const ansiRegex = require('ansi-regex')
+const path = require('path')
+
 module.exports = {
   configureWebpack: {
-    entry: ['babel-polyfill', '@/src/main.js'],
+    entry: ['@babel/polyfill', './src/main.js'],
   },
+  transpileDependencies: [ansiRegex],
 }
