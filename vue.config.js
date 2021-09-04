4612ca88f040dde4f15c8f8caeef69900321b36d
iduk <iduk.design@gmail.com>
2021-09-04 17:58:41 +0900

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 const ansiRegex = require('ansi-regex')
-const path = require('path')
 
 module.exports = {
   configureWebpack: {
-    entry: ['@babel/polyfill', './src/main.js'],
+    entry: ['babel-polyfill', './src/main.js'],
   },
   transpileDependencies: [ansiRegex],
 }
