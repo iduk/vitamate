1f748a3cd32b26e5610bbcca59688011a0136fd5
iduk <iduk.design@gmail.com>
2021-09-06 08:09:16 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,10 +10,10 @@ import Sidebar from './layouts/Sidebar'
 import Fluid from './layouts/Fluid'
 import Tung from './layouts/Tung'
 
-Vue.component('layout-default', Default)
-Vue.component('layout-sidebar', Sidebar)
-Vue.component('layout-fluid', Fluid)
-Vue.component('layout-tung', Tung)
+Vue.component('default', Default)
+Vue.component('sidebar', Sidebar)
+Vue.component('fluid', Fluid)
+Vue.component('tung', Tung)
 
 Vue.config.productionTip = false
 
