9228811d61baf6608dc7a0a4552851f49b3f38cc
iduk <iduk.design@gmail.com>
2021-09-16 08:52:42 +0900

    revert

--- components/Layouts/LayoutSidebar.js
+++ components/Layouts/LayoutSidebar.js
@@ -1,14 +1,12 @@
-import Link from 'next/link'
-import styled from 'styled-components'
 import Sidebar from './Sidebar'
 
-export default function LayoutSidebar({ Component, pageProps }) {
+export default function LayoutSidebar({ children }) {
   return (
     <section className={'container mx-auto max-w-5xl flex flex-initial'}>
       <Sidebar />
       <div className="w-full px-16 py-16">
         <h1 className=" text-4xl font-bold pb-12">서브제목입니다</h1>
-        <Component {...pageProps} />
+        {children}
       </div>
     </section>
   )
--- components/Layouts/Sidebar.js
+++ components/Layouts/Sidebar.js
@@ -1,29 +1,49 @@
 import Link from 'next/link'
-import styled from 'styled-components'
-import navs from './navs'
 
 export default function Sidebar() {
-  const sidebarStyles = styled.div`
-    #sidebar {
-      position: relative;
-      &::before {
-        content: '';
-        position: absolute;
-        right: 0;
-        top: 0;
-        width: 1px;
-        height: calc(100vh - 110px);
-        @apply bg-gray-200;
-      }
-    }
-  `
+  const news = [
+    {
+      path: '/news',
+      title: '비타 뉴스',
+    },
+    {
+      path: '/news/whitepaper',
+      title: '비타 백서',
+    },
+  ]
+  const mypage = [
+    {
+      path: '/mypage',
+      title: '내 정보',
+    },
+    {
+      path: '/mypage/subscribe',
+      title: '구독 관리',
+    },
+    {
+      path: '/mypage/addressbook',
+      title: '배송지 관리',
+    },
+    {
+      path: '/mypage/calendar',
+      title: '비타 캘린더',
+    },
+    {
+      path: '/mypage/point',
+      title: '포인트',
+    },
+    {
+      path: '/mypage/pointshop',
+      title: '포인트샵',
+    },
+  ]
 
   return (
     <aside id="sidebar" className="flex-shrink-0 w-52 p-4 py-16 my-0 text-center">
-      <nav>
-        <h3 className="text-xl font-bold">{navs.pages.title}</h3>
+      <nav id="mypage">
+        <h3 className="text-xl font-bold">현재페이지 타이틀</h3>
         <ul className="mt-6">
-          {navs.pages.map((nav) => (
+          {news.map((nav) => (
             <li className="py-3" key={nav.id}>
               <Link className={'nav'} href={nav.path}>
                 {nav.title}
