a340fdc8b2b4e4bea0ed1c8a5921feeeb09bda0d
iduk <iduk.design@gmail.com>
2021-09-17 01:08:53 +0900

    feat Sidebar...SubPages..

--- pages/mypage/pointshop.js
+++
@@ -1,9 +0,0 @@
-import LayoutSidebar from 'components/Layouts/LayoutSidebar'
-
-export default function PointShop() {
-  return (
-    <LayoutSidebar>
-      <h1>구독관리</h1>
-    </LayoutSidebar>
-  )
-}
