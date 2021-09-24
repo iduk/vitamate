9f61ffeb1c2899c676f6644d20b3dcf72bff700f
iduk <iduk.design@gmail.com>
2021-09-24 16:58:40 +0900

    fix 모바일 재배치>배송지관리

--- pages/mypage/addressbook.js
+++ pages/mypage/addressbook.js
@@ -1,211 +1,75 @@
 import ContainerAside from 'components/Layouts/ContainerAside'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Addressbook() {
-  const th = [
-    { title: '배송지명', align: 'left', size: 160 },
-    { title: '주소', align: 'left', size: 330 },
-    { title: '연락처', align: 'left', size: 180 },
-    { title: '수정/삭제', align: 'left', size: '' },
+  const [isSelected, setIsSelected] = useState(false)
+
+  const addressData = [
+    {
+      addressName: '강남 아파트1',
+      zipcode: '04348',
+      address: '서울특별시 강남구 테헤란로 33길 에라타워 18-19',
+      tel: '02-0000-0000',
+    },
+    {
+      addressName: '국립한글박물관',
+      zipcode: '04348',
+      address: '서울특별시 용산구 이태원로55길 3-10(한남동)',
+      tel: '02-0000-0000',
+    },
   ]
+
   return (
     <ContainerAside>
       <h1 className="page-title _sub">배송지 관리</h1>
 
       <article>
-        <Link href="#">
-          <a className="mt-6 flex justify-between px-8 py-8 border border-gray-300 rounded-lg hover:border-gray-500 hover:shadow hover:transition duration-200 ease-in-out">
-            <div>
+        {addressData.map((data, id) => (
+          <Link key={id} href="#">
+            <a
+              onClick={() => setIsSelected(!isSelected)}
+              className={`${
+                isSelected === true ? 'border-gray-600 shadow' : null
+              } grid place-content-between mt-6 p-6 border border-gray-300 rounded-lg transition duration-200 ease-in-out`}
+            >
               <dl>
-                <dt className="mb-2 text-xl leading-none font-bold flex items-center">
-                  <span className="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-primary-600 border border-primary-600 rounded">
-                    기본배송지
-                  </span>
-                  나의강남권아파트
+                <dt className="mb-2 flex justify-between items-center text-xl leading-none font-bold">
+                  {/* -- 기본배송지 표기 -- */}
+                  <span>{data.addressName}</span>
+                  <div>
+                    <span className="mb-1 inline-flex items-center justify-center px-2 py-1 text-sm font-bold leading-none text-primary-600 border border-primary-600 rounded">
+                      기본배송지
+                    </span>
+                  </div>
                 </dt>
                 <dd className="text-lg">
-                  <span className="text-primary-600 mr-2">04348</span>
-                  서울특별시 용산구 이태원로55길 3-10(한남동)
+                  <span className="text-primary-600 mr-2">{data.zipcode}</span>
+                  {data.address}
                 </dd>
-                <dd className="flex items-center text-gray-600 mt-4">
-                  <span className="text-gray-400 text-sm mr-3">연락처</span> 010-0000-0000
+                <dd className="flex items-center text-gray-600 mt-2">
+                  <span className="text-gray-400 text-sm mr-2">연락처</span>
+                  {data.tel}
                 </dd>
-              </dl>
-            </div>
-            {/* ---- 수정,삭제 ---- */}
-            <div>
-              <button className="py-2 px-5 rounded border text-base border-gray-300 text-gray-600 hover:bg-gray-100">
-                수정
-              </button>
-              <span className="mx-1"></span>
-              <button className="py-2 px-5 rounded border text-base border-gray-300 text-gray-600 hover:border-gray-400">
-                삭제
-              </button>
-            </div>
-          </a>
-        </Link>
-        <Link href="#">
-          <a className="mt-6 flex justify-between px-8 py-8 border border-gray-300 rounded-lg hover:border-gray-500 hover:shadow hover:transition duration-200 ease-in-out">
-            <div>
-              <dl>
-                <dt className="mb-2 text-xl leading-none font-bold flex items-center">나의강남권아파트</dt>
-                <dd className="text-lg">
-                  <span className="text-primary-600 mr-2">04348</span>
-                  서울특별시 용산구 이태원로55길 3-10(한남동)
-                </dd>
-                <dd className="flex items-center text-gray-600 mt-4">
-                  <span className="text-gray-400 text-sm mr-3">연락처</span> 010-0000-0000
+                <dd className="mt-4">
+                  <button className="py-1 px-4 rounded border text-base border-gray-300 text-gray-600 hover:bg-gray-100">
+                    수정
+                  </button>
                 </dd>
               </dl>
-            </div>
-            {/* ---- 수정,삭제 ---- */}
-            <div>
-              <button className="py-2 px-5 rounded border text-base border-gray-300 text-gray-600 hover:bg-gray-100">
-                수정
-              </button>
-              <span className="mx-1"></span>
-              <button className="py-2 px-5 rounded border text-base border-gray-300 text-gray-600 hover:border-gray-400">
-                삭제
-              </button>
-            </div>
-          </a>
-        </Link>
-        <Link href="#">
-          <a className="mt-6 flex justify-between px-8 py-8 border border-gray-300 rounded-lg hover:border-gray-500 hover:shadow hover:transition duration-200 ease-in-out">
-            <div>
-              <dl>
-                <dt className="mb-2 text-xl leading-none font-bold flex items-center">나의강남권아파트</dt>
-                <dd className="text-lg">
-                  <span className="text-primary-600 mr-2">04348</span>
-                  서울특별시 용산구 이태원로55길 3-10(한남동)
-                </dd>
-                <dd className="flex items-center text-gray-600 mt-4">
-                  <span className="text-gray-400 text-sm mr-3">연락처</span> 010-0000-0000
-                </dd>
-              </dl>
-            </div>
-            {/* ---- 수정,삭제 ---- */}
-            <div>
-              <button className="py-2 px-5 rounded border text-base border-gray-300 text-gray-600 hover:bg-gray-100">
-                수정
-              </button>
-              <span className="mx-1"></span>
-              <button className="py-2 px-5 rounded border text-base border-gray-300 text-gray-600 hover:border-gray-400">
-                삭제
-              </button>
-            </div>
-          </a>
-        </Link>
+            </a>
+          </Link>
+        ))}
       </article>
-      <div className="my-6 text-right">
-        <button className="btn border border-primary-600 text-primary-600 hover:border-primary-700 hover:text-primary-700 hover:shadow">
-          기본 배송지로 등록
-        </button>
-        <button className="btn bg-primary-600 text-white hover:bg-primary-700 ml-3">신규 배송지 등록</button>
-      </div>
 
-      <div className="mt-20" style={{ display: 'none' }}>
-        <table className="min-w-full leading-normal border-b border-gray-200">
-          <thead>
-            <tr>
-              {th.map((label, id) => (
-                <th
-                  key={id}
-                  className="py-4 pr-8 font-bold text-gray-900"
-                  style={{
-                    textAlign: label.align,
-                    width: label.size,
-                  }}
-                >
-                  {label.title}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="border-t border-gray-200">
-              {/*--------- 배송지 ---------*/}
-              <td className="py-5 pr-8 align-top">
-                <span className="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-primary-600 border border-primary-600 rounded">
-                  기본배송지
-                </span>
-                <p>국립한글박물관</p>
-              </td>
-              {/*--------- 주소 ---------*/}
-              <td className="py-5 pr-8 align-top">
-                <span className="text-gray-400">04383</span>
-                <p>서울특별시 용산구 서빙고로 139(용산동6가)</p>
-              </td>
-
-              {/*--------- 연락처 ---------*/}
-              <td className="py-5 pr-8 align-top">
-                <p>010-1234-5678</p>
-              </td>
-              {/*--------- 수정/삭제 ---------*/}
-              <td className="py-5 align-top">
-                <button className="py-2 px-5 rounded border text-base border-gray-300 text-gray-600 hover:bg-gray-100">
-                  수정
-                </button>
-                <span className="mx-1"></span>
-                <button className="py-2 px-5 rounded border text-base border-gray-300 text-danger-600 hover:bg-danger-50">
-                  삭제
-                </button>
-              </td>
-            </tr>
-            <tr className="border-t border-gray-200">
-              {/*--------- 배송지 ---------*/}
-              <td className="py-5 pr-8 align-top">
-                <p>회사</p>
-              </td>
-              {/*--------- 주소 ---------*/}
-              <td className="py-5 pr-8 align-top">
-                <span className="text-gray-400">04348</span>
-                <p>서울특별시 용산구 이태원로55길 3-10(한남동)</p>
-              </td>
-
-              {/*--------- 연락처 ---------*/}
-              <td className="py-5 pr-8 align-top">
-                <p>010-1234-5678</p>
-              </td>
-              {/*--------- 수정/삭제 ---------*/}
-              <td className="py-5 align-top">
-                <button className="py-2 px-5 rounded border text-base border-gray-300 text-gray-600 hover:bg-gray-100">
-                  수정
-                </button>
-                <span className="mx-1"></span>
-                <button className="py-2 px-5 rounded border text-base border-gray-300 text-danger-600 hover:bg-danger-50">
-                  삭제
-                </button>
-              </td>
-            </tr>
-            <tr className="border-t border-gray-200">
-              {/*--------- 배송지 ---------*/}
-              <td className="py-5 pr-8 align-top">
-                <p>여긴어디</p>
-              </td>
-              {/*--------- 주소 ---------*/}
-              <td className="py-5 pr-8 align-top">
-                <span className="text-gray-400">30116</span>
-                <p>세종특별자치시 한누리대로 411(어진동)</p>
-              </td>
-
-              {/*--------- 연락처 ---------*/}
-              <td className="py-5 pr-8 align-top">
-                <p>010-1234-5678</p>
-              </td>
-              {/*--------- 수정/삭제 ---------*/}
-              <td className="py-5 align-top">
-                <button className="py-2 px-5 rounded border text-base border-gray-300 text-gray-600 hover:bg-gray-100">
-                  수정
-                </button>
-                <span className="mx-1"></span>
-                <button className="py-2 px-5 rounded border text-base border-gray-300 text-danger-600 hover:bg-danger-50">
-                  삭제
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="flex justify-end my-6">
+        {/* ---- 기본배송지등록,신규배송지등록 ---- */}
+        <div>
+          <button className="btn border border-primary-600 text-primary-600 hover:border-primary-700 hover:text-primary-700 hover:shadow">
+            기본 배송지로 등록
+          </button>
+          <button className="btn bg-primary-600 text-white hover:bg-primary-700 ml-3">신규 배송지 등록</button>
+        </div>
       </div>
     </ContainerAside>
   )
