e6165f2991311e6a62396478eda1ab665ec4bd13
iduk <iduk.design@gmail.com>
2021-09-25 14:23:30 +0900

    Delete calendar.js

--- pages/mypage/calendar.js
+++
@@ -1,9 +0,0 @@
-import ContainerAside from 'components/Layouts/ContainerAside'
-
-export default function Calendar() {
-  return (
-    <ContainerAside>
-      <h1>구독관리</h1>
-    </ContainerAside>
-  )
-}
