b97d5001a3082dd2dbf40845e8c29df2b0805f17
iduk <iduk.design@gmail.com>
2021-09-30 09:36:27 +0900

    test

--- pages/test.js
+++ pages/test.js
@@ -1,18 +1,34 @@
-const Test = () => {
-  const [items, setItems] = useState(['one', 'two', 'three', 'four', 'five'])
-  const [colors, setColors] = useState(['red', 'blue', 'green', 'yellow', 'pink'])
+import React from 'react'
+import Link from 'next/link'
+import { withRouter } from 'next/router'
 
-  function shuffle() {
-    setItems(_.shuffle(items))
-  }
+const Tabs = ({ router }) => {
+  const {
+    query: { tab },
+  } = router
+
+  const isTabOne = tab === '1' || tab == null
+  const isTabTwo = tab === '2'
 
   return (
     <div>
-      <button onClick={shuffle}>Shuffle</button>
-      <CheckableList items={items} />
-      <CheckableList items={colors} renderItem={(itemText) => <span style={{ color: itemText }}>{itemText}</span>} />
+      <div selected={isTabOne}>
+        <Link href={{ pathname: '/mypage/subscribe', query: { tab: '1' } }}>
+          <a>Tab 1</a>
+        </Link>
+      </div>
+      <div selected={isTabTwo}>
+        <Link href={{ pathname: '/mypage/unsubscribe', query: { tab: '2' } }}>
+          <a>Tab 2</a>
+        </Link>
+      </div>
+
+      <section className="mt-10">
+        {isTabOne && <React.Fragment>This is tab one content</React.Fragment>}
+        {isTabTwo && <React.Fragment>This is tab two content</React.Fragment>}
+      </section>
     </div>
   )
 }
 
-export default Test
+export default withRouter(Tabs)
