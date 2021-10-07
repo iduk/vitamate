e57be84f39b7d578b486d58df0c4873bf9d11301
iduk <iduk.design@gmail.com>
2021-10-07 21:15:56 +0900

    fix 서비스 약관 -> 이용약관

--- pages/TermsOfService.js
+++ pages/TermsOfService.js
@@ -4,7 +4,7 @@ export default function TermsOfService() {
   return (
     <ContainerFluid>
       <div className={'container py-12 px-6 lg:px-0 lg:py-16'}>
-        <h1 className="page-title">서비스 약관</h1>
+        <h1 className="page-title">이용약관</h1>
       </div>
     </ContainerFluid>
   )
