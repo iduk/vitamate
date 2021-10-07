ba36229a23ce8c9a6fb85509f63991ae65e9d00e
iduk <iduk.design@gmail.com>
2021-10-08 01:46:09 +0900

    feat 구독UI변경

--- pages/mypage/subscribe/index.js
+++ pages/mypage/subscribe/index.js
@@ -12,46 +12,64 @@ import Unsubscribe from './unsubscribe'
 // 구독 상품
 const productData = [
   {
-    thumbImg:
-      'https://images.pexels.com/photos/3683068/pexels-photo-3683068.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    name: '감마리놀렌산 메이트',
-    price: '998,000',
-    listPrice: '998,000',
+    thumbImg: '/images/product-bio.png',
+    name: '프로바이옴메이트',
+    property: '300mg 1캡슐',
+    content: '유익균 증식과 유해균 억제, 장건강 개선',
+    price: '7,500',
+    listPrice: '9,000',
   },
   {
-    thumbImg:
-      'https://images.pexels.com/photos/3683047/pexels-photo-3683047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    name: '퓨어코어 종합 멀티 비타민 미네랄',
-    price: '998,000',
-    listPrice: '998,000',
+    thumbImg: '/images/product-lutein.png',
+    name: '루테인메이트',
+    property: '150mg 1캡슐',
+    content: '눈 건강 개선에 도움',
+    price: '8,000',
+    listPrice: '9,500',
   },
   {
-    thumbImg:
-      'https://images.pexels.com/photos/9742764/pexels-photo-9742764.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    thumbImg: '/images/product-magnesium.png',
+    name: '마그네슘메이트',
+    property: '1000mg 1정',
+    content: '인체 에너지 이용, 신경과 근육 기능 유지에 필요',
+    price: '8,000',
+    listPrice: '9,500',
+  },
+  {
+    thumbImg: '/images/product-omega.png',
+    name: '오메가메이트',
+    property: '760mg 2캡슐',
+    content: '혈행 개선에 도움, 기억력 개선, 눈 건강 개선에 도움',
+    price: '8,000',
+    listPrice: '9,500',
+  },
+  {
+    thumbImg: '/images/product-Q10.png',
+    name: '코엔자임메이트',
+    property: '300mg 1캡슐',
+    content: '고혈압 개선에 도움, 항산화효과',
+    price: '8,000',
+    listPrice: '9,500',
+  },
+  {
+    thumbImg: '/images/product-theanine.png',
     name: '테아닌메이트',
-    price: '998,000',
-    listPrice: '998,000',
+    property: '1000mg 1정',
+    content: '긴장 완화에 도움',
+    price: '8,000',
+    listPrice: '9,500',
   },
 ]
-// 구독 예정 상품
-const productEstData = [
+
+const productList = [
   {
-    thumbImg:
-      'https://images.pexels.com/photos/2622186/pexels-photo-2622186.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    name: '다노 이뮨멀티비타민',
-    price: '10,300',
-  },
-  {
-    thumbImg:
-      'https://images.pexels.com/photos/9742851/pexels-photo-9742851.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    name: '퓨어코어 종합 멀티 비타민 미네랄',
-    price: '9,300',
-  },
-  {
-    thumbImg:
-      'https://images.pexels.com/photos/2622186/pexels-photo-2622186.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-    name: '밸런스 비타민 B(2개월)',
-    price: '8,900',
+    id: 0,
+    productName: 'B메이트',
+    productTake1: '640mg 1정',
+    productImg:
+      'https://images.pexels.com/photos/3683036/pexels-photo-3683036.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    productEff1: '체내 에너지 생성과 각종 대사에 필요',
+    productEff2: '8가지 종류의 비타민B 성분',
   },
 ]
 
@@ -102,144 +120,149 @@ const TabOne = () => {
   const [showModal, setShowModal] = useState(false)
   const [toCart, setToCart] = useState(false)
 
-  useEffect(() => {}, [])
+  const isShowing = (e) => {
+    e.preventDefault()
+    setShowModal({ ...showModal, id: e.target.id })
+  }
 
   return (
-    <div className="flex justify-between items-start">
+    <div>
       <section className="w-full">
-        <article className="mb-10 lg:mr-10">
-          <h2 className="text-xl mb-2">
-            <span>이병윤</span>님을 위한 맞춤 추천 상품!
-          </h2>
+        <article className="grid grid-cols-2 gap-8">
+          <div>
+            <h2 className="text-2xl font-bold mb-4">
+              <span>이병윤</span>님을 위한 <br />
+              알고리즘 추천 영양제!
+            </h2>
+            <p className="text-sm text-primary-600">*1년 이상 약정 선택시 유전자검사 비용 전액을 페이백 해드립니다 </p>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <Select options={paymentType} />
+            <Select options={paymentType_a} />
+          </div>
+        </article>
 
-          <ul>
+        <ul className="mt-5">
+          {productData.map((product, id) => (
+            <li key={id} className={'flex py-5 border-b border-gray-200 relative'}>
+              <Link href="#">
+                <a
+                  onClick={() => setShowModal(true)}
+                  className="thumb thumb-large border overflow-hidden relative flex-shrink-0"
+                >
+                  <Img className="rounded-md" src={product.thumbImg} layout="fill" alt={product.name} />
+                </a>
+              </Link>
+              <dl className="ml-8 py-2 flex-1">
+                <dt className="text-xl flex align-items-center">
+                  <p>{product.name}</p>
+                  <div className="grid place-content-center ml-2">
+                    <span className="px-1 py-1 text-xs font-bold leading-none text-primary-600 border border-primary-600 rounded">
+                      구독중
+                    </span>
+                  </div>
+                </dt>
+                <dd className="text-sm my-1">{product.property}</dd>
+                <dd className="text-base text-gray-400 mb-3">{product.content}</dd>
+                <dd>
+                  <div className="flex items-baseline">
+                    <p className="text-lg font-bold">
+                      <span>{product.price}</span>원
+                    </p>
+                    <p className={'text-base ml-2 line-through text-gray-400'}>
+                      <span>{product.listPrice}</span>원
+                    </p>
+                  </div>
+                </dd>
+              </dl>
+              <div className="flex-shrink-0 flex items-center justify-end lg:ml-5">
+                {toCart === true ? (
+                  <button
+                    onClick={() => setToCart(!toCart)}
+                    className={'btn border border-gray-300 text-gray-400 hover:bg-gray-50'}
+                  >
+                    <>
+                      <span className="pr-2">-</span>
+                      <span>구독취소</span>
+                    </>
+                  </button>
+                ) : (
+                  <>
+                    <button
+                      onClick={() => setToCart(!toCart)}
+                      className={'btn border border-primary-600 text-primary-600 hover:bg-primary-100'}
+                    >
+                      <span className="pr-2">+</span>
+                      <span>구독하기</span>
+                    </button>
+                  </>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+
+        <article className="my-10 p-6 h-auto rounded-lg bg-primary-100">
+          <h2 className="text-xl mb-6">그 밖의 훌륭한 상품!</h2>
+          <ul className="grid grid-cols-1 lg:grid-cols-2 gap-8">
             {productData.map((product, id) => (
-              <li key={id} className={'flex py-5 border-b border-gray-200 relative'}>
-                <Link href="#">
-                  <a onClick={() => setShowModal(true)} className="thumb flex-shrink-0">
-                    <Img className="rounded-md" src={product.thumbImg} width={100} height={100} alt={product.name} />
-                  </a>
-                </Link>
-                <dl className="ml-5 flex-1">
-                  <dt className="text-base text-gray-500 mb-2">{product.name}</dt>
-                  <dd>
-                    <div className="flex items-end">
-                      <p className="text-xl font-bold">
+              <li key={id} className="rounded-large bg-white p-8">
+                <div className="flex justify-between">
+                  <div className="w-full">
+                    <h6 className="text-xl font-bold leading-snug">{product.name}</h6>
+                    <p className="text-sm">{product.property}</p>
+                    <div className="flex items-baseline mt-3">
+                      <p className="text-lg font-bold">
                         <span>{product.price}</span>원
                       </p>
                       <p className={'text-base ml-3 line-through text-gray-400'}>
                         <span>{product.listPrice}</span>원
                       </p>
                     </div>
-                  </dd>
-                </dl>
-                <div className="flex-shrink-0 flex items-end justify-end">
-                  <button onClick={() => setToCart(!toCart)} type="button">
-                    <Img
-                      src={`/images/btn-${toCart === true ? 'plus' : 'minus'}.svg`}
-                      width={40}
-                      height={40}
-                      alt="plus"
-                    />
+                  </div>
+                  <div className="ml-8 flex-shrink-0">
+                    <span
+                      className="thumb bg-cover bg-no-repeat bg-center"
+                      style={{
+                        backgroundImage: `url(${product.thumbImg})`,
+                      }}
+                    ></span>
+                  </div>
+                </div>
+
+                <div className="text-base leading-normal text-gray-500 mt-6 overflow-hidden">
+                  <p className="h-11">{product.content}</p>
+                </div>
+
+                <div className="mt-6">
+                  <button
+                    onClick={isShowing}
+                    className="w-full p-3 rounded-md text-primary-600 border border-primary-600 hover:bg-primary-600 hover:text-white transition-all"
+                  >
+                    <span className="pr-2">+</span>
+                    <span>구독하기</span>
                   </button>
                 </div>
               </li>
-            ))}
-          </ul>
-        </article>
-
-        <article className="mb-10 lg:mr-10 p-6 h-auto rounded-lg bg-primary-100">
-          <h2 className="text-xl mb-2">그 밖의 훌륭한 상품!</h2>
-
-          <ul>
-            {productData.map((product, id) => (
-              <>
-                <li key={id} className={'flex p-5 relative rounded-lg bg-white mt-4'}>
-                  <Link href="#">
-                    <a onClick={() => setShowModal(true)} className="thumb flex-shrink-0">
-                      <Img className="rounded-md" src={product.thumbImg} width={100} height={100} alt={product.name} />
-                    </a>
-                  </Link>
-                  <dl className="ml-5 flex-1">
-                    <dt className="text-base text-gray-500 mb-2">{product.name}</dt>
-                    <dd>
-                      <div className="flex items-end">
-                        <p className="text-xl font-bold">
-                          <span>{product.price}</span>원
-                        </p>
-                        <p className={'text-base ml-3 line-through text-gray-400'}>
-                          <span>{product.listPrice}</span>원
-                        </p>
-                      </div>
-                    </dd>
-                  </dl>
-                  <div className="flex-shrink-0 flex items-end justify-end">
-                    <button onClick={() => setToCart(!toCart)} type="button">
-                      <Img
-                        src={`/images/btn-${toCart === true ? 'plus' : 'minus'}.svg`}
-                        width={40}
-                        height={40}
-                        alt="plus"
-                      />
-                    </button>
-                  </div>
-                </li>
-              </>
             ))}
           </ul>
         </article>
       </section>
 
-      <aside className="hidden lg:block w-1/3 p-5 bg-gray-100 rounded-md lg:flex-shrink-0 lg:shadow">
-        <h2 className="text-base">구독 예정 상품</h2>
-        <ul>
-          {productEstData.map((product, id) => (
-            <>
-              <li key={id} className="py-3 flex border-b border-gray-200">
-                <Link href="#">
-                  <a onClick={() => setShowModal(true)} className="thumb thumb-small flex-shrink-0">
-                    <Img className="rounded-md" src={product.thumbImg} width={64} height={64} alt={product.name} />
-                  </a>
-                </Link>
-
-                <dl className="ml-4 flex-1">
-                  <dt className="text-xs leading-tight text-gray-500 mb-2">{product.name}</dt>
-                  <dd>
-                    <div className="flex items-end">
-                      <p className="text-base font-bold">
-                        <span>{product.price}</span>원
-                      </p>
-                    </div>
-                  </dd>
-                </dl>
-                <div className="flex-shrink-0 flex items-center justify-end">
-                  <button className="relative p-1">
-                    <Img src="/images/btn-popup-cancel.svg" width={24} height={24} alt="remove" />
-                  </button>
-                </div>
-              </li>
-            </>
-          ))}
-          <li className="mt-3">
-            <Select options={paymentType} />
-          </li>
-          <li className="mt-2">
-            <Select options={paymentType_a} />
-          </li>
-        </ul>
+      <footer>
         {/* ----- 정산 ----- */}
-        <footer>
-          <div className="flex justify-between mt-4">
-            <h6 className={'font-bold'}>총 구독수</h6>
+        <article className="w-full p-5 bg-gray-100 rounded-md lg:flex-shrink-0 lg:shadow">
+          <div className="flex justify-between">
+            <h6 className={'font-bold'}>총 구독상품</h6>
+
             <p>
-              <span>2</span>개
+              <span>2</span>개<span className="px-2">/</span>
+              <span>19,600</span>원
             </p>
           </div>
           <div className="flex justify-between mt-2">
-            <h6 className={'font-bold'}>총 구독가</h6>
-            <p>
-              <span>19,600</span>원
-            </p>
+            <h6 className={'font-bold'}>약정기간</h6>
+            <p>1년</p>
           </div>
           <div className="flex justify-between mt-2">
             <h6 className={'font-bold'}>할인금액</h6>
@@ -253,12 +276,18 @@ const TabOne = () => {
               <span>10,000</span>원
             </p>
           </div>
-          {/* ---- footer ---- */}
-          <div className="mt-6">
-            <button className="w-full rounded px-4 py-4 bg-primary-600 text-white text-lg">구독 시작</button>
+        </article>
+        <div className="mt-6 grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <Link href="#">
+            <a className="p-3 place-self-center mr-auto text-gray-600 underline hover:text-black">
+              정기구독을 <b>일시 중지</b>하시겠습니까?
+            </a>
+          </Link>
+          <div className="text-right">
+            <button className="rounded px-6 py-4 w-full lg:w-56 bg-primary-600 text-white text-lg">결제하기</button>
           </div>
-        </footer>
-      </aside>
+        </div>
+      </footer>
 
       <Modal title={'제품보기'} onClose={() => setShowModal(false)} show={showModal} size={'md'}>
         <div>
