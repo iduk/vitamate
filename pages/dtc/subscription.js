76f4e70f1207d2ddf7b69abaad8711eabf479a15
iduk <iduk.design@gmail.com>
2021-10-10 14:15:16 +0900

    fix 유전자검사 신청 팝업 수정

--- pages/dtc/subscription.js
+++ pages/dtc/subscription.js
@@ -15,26 +15,38 @@ export default function Subscription() {
     { id: 'hahaha', value: 'HOHOHO', label: '선택' },
     { id: 'hahaha', value: 'HOHOHO', label: '010' },
   ]
+  const store_date = [
+    { id: 'store-date1', value: 'store1', label: '유전자 검사 분석 후 폐기해 주세요 ' },
+    { id: 'store-date2', value: 'store2', label: '회원 탈퇴시 자동 폐기해 주세요 ' },
+  ]
+  const sheet_types = [
+    { id: 'type1', value: 'type1', label: '무료 PDF 보기' },
+    { id: 'type2', value: 'type2', label: '유료 책자 신청 15,000원' },
+  ]
+
   const birthdayType = [{ label: '양력' }, { label: '음력' }]
   const birthdayMonth = [{ label: '1월' }, { label: '2월' }]
   const birthdayDay = [{ label: '1일' }, { label: '2일' }]
 
   return (
-    <>
-      <article className={'overflow-y-auto'}>
+    <div className="flex flex-col justify-between">
+      <article className="overflow-y-auto border-b" style={{ height: '65vh' }}>
         <form onSubmit={registerUser}>
           <div className="mb-4 p-4 leading-normal text-secondary-700 bg-secondary-50 rounded-lg" role="alert">
             <p>1년 이상 약정 선택시 유전자검사 비용 전액을 페이백 해드립니다.</p>
           </div>
-          <div className="form-group">
-            <TextInput type="text" label="이름" id="userPhone" placeholder="이름을 입력해주세요" />
-          </div>
 
-          <div className="form-group">
-            <label>성별</label>
-            <div className="grid grid-cols-2 gap-4">
-              <button className={'w-full p-3 rounded-md border border-primary-600 text-primary-600 '}>남자</button>
-              <button className={'p-3 rounded-md border border-gray-300 text-gray-600 '}>여자</button>
+          <div className="form-group grid grid-cols-8 gap-4">
+            <div className=" col-span-5">
+              <TextInput type="text" label="이름" id="userPhone" placeholder="이름을 입력해주세요" />
+            </div>
+
+            <div className=" col-span-3">
+              <label>성별</label>
+              <div className="grid grid-cols-2 gap-2">
+                <button className={'w-full p-3 rounded-md border border-primary-600 text-primary-600 '}>남자</button>
+                <button className={'p-3 rounded-md border border-gray-300 text-gray-600 '}>여자</button>
+              </div>
             </div>
           </div>
 
@@ -77,6 +89,14 @@ export default function Subscription() {
             <TextInput type="text" placeholder="나머지 주소를 입력해주세요" id="userAddress2" className="mt-3" />
           </div>
 
+          <div className="form-group">
+            <Select label="유전자검사 결과지 유형 선택" options={sheet_types} />
+          </div>
+
+          <div className="form-group">
+            <Select label="유전자검사 결과지 보관기간 선택" options={store_date} />
+          </div>
+
           <ul className="grid grid-cols-2 p-4 mt-6 border-t border-b border-gray-200 bg-gray-100">
             <li>
               <h5 className="text-lg font-bold">유전자 검사</h5>
@@ -98,14 +118,14 @@ export default function Subscription() {
                 <a className="p-2 text-sm text-gray-500 underline">보기</a>
               </Link>
             </li>
-            <li className="flex justify-between items-center">
+            {/* <li className="flex justify-between items-center">
               <Checkbox id="check-3" name="agree" label="책자 신청하기 (+20,000원)" />
-            </li>
+            </li> */}
           </ul>
         </form>
       </article>
 
-      <footer className={'flex justify-center p-0'}>
+      <footer className={'flex justify-center pt-6'}>
         <button
           type="submit"
           onClick={() => setShowModal(false)}
@@ -115,6 +135,6 @@ export default function Subscription() {
           결제하기
         </button>
       </footer>
-    </>
+    </div>
   )
 }
