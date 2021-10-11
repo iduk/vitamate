b4aeb7c83eaef01b0bb36bde4855578b5679007c
iduk <iduk.design@gmail.com>
2021-10-11 19:07:22 +0900

    feat Input Validate UI, 샘플 전달

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,42 @@
 import { useState } from 'react'
+import TextInput from 'components/Forms/TextInput'
 
 export default function Test() {
-  return <div></div>
+  return (
+    <section className=" max-w-lg mx-auto py-20">
+      <h1 className="py-4 text-2xl">Input Validate UI</h1>
+      <hr className="mt-6" />
+
+      {/* Error */}
+      <h1 className="inline-block text-xl font-extrabold my-8">Input Error</h1>
+      <div className="form-group error">
+        <TextInput
+          type="password"
+          label="비밀번호"
+          id="userPassword"
+          value="123456!@#&%"
+          placeholder="영어 대문자, 소문자, 숫자, 특수문자 중 2종류 이상을 조합하여 최소 8자리 이상 입력"
+        >
+          <p>8자리 이상 입력해주세요.</p>
+        </TextInput>
+      </div>
+      <hr className="mt-6" />
+
+      {/* Success */}
+      <h1 className="inline-block text-xl font-extrabold my-8">Input Success</h1>
+      <div className="form-group success">
+        <TextInput
+          type="password"
+          label="비밀번호"
+          id="userPassword"
+          value="123456!@#&%"
+          placeholder="영어 대문자, 소문자, 숫자, 특수문자 중 2종류 이상을 조합하여 최소 8자리 이상 입력"
+        >
+          <p>와우 통과!</p>
+        </TextInput>
+      </div>
+    </section>
+  )
 }
 
 // const Data = [
