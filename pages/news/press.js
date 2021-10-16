f90eb62325673031e973b8d16d6e6025a3406c88
iduk <iduk.design@gmail.com>
2021-10-16 12:54:41 +0900

    fix 언론보도 모바일 대응

--- pages/news/press.js
+++ pages/news/press.js
@@ -15,7 +15,7 @@ export default function Press() {
             {/* title */}
             <Link href="https://www.fnnews.com/news/202108261011010605">
               <a target="_blank" className="w-10/12 font-normal">
-                <div className="flex items-center cursor-pointer">
+                <div className="block lg:flex items-center cursor-pointer">
                   <span className="w-28 text-sm text-gray-500">2021.08.26</span>
                   <p>비타메이트, 유전자 분석으로 맞춤 영양제 제공 서비스 출시</p>
                 </div>
@@ -26,7 +26,7 @@ export default function Press() {
             {/* title */}
             <Link href="https://news.mt.co.kr/mtview.php?no=2021082514320068577">
               <a target="_blank" className="w-10/12 font-normal">
-                <div className="flex items-center cursor-pointer">
+                <div className="block lg:flex items-center cursor-pointer">
                   <span className="w-28 text-sm text-gray-500">2021.08.25</span>
                   <p>개인 맞춤형 영양제 큐레이션 '비타메이트', 'K-스타트업대상'</p>
                 </div>
