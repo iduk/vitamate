9e4dece52b3bd9aab395e7f395cc9e12af485e3c
iduk <iduk.design@gmail.com>
2021-10-28 02:08:41 +0900

    feat 늦었지만 파비콘..

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,6 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
-        {/* <meta name="viewport" content="viewport-fit=cover" /> */}
       </Head>
       <Layout>
         <Component {...pageProps} />
