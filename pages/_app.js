a61def0d031d01727c8757ace7e52ecfaad4acdf
iduk <iduk.design@gmail.com>
2021-11-07 15:46:46 +0900

    fix 모달 Style 수정6

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,6 @@ import 'styles/globals.scss'
 import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    let vh = window.innerHeight * 0.01
-
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
-
-    window.addEventListener('resize', () => {
-      console.log('resize')
-      let vh = window.innerHeight * 0.01
-      document.documentElement.style.setProperty('--vh', `${vh}px`)
-    })
-  }, [])
   return (
     <>
       <Head>
